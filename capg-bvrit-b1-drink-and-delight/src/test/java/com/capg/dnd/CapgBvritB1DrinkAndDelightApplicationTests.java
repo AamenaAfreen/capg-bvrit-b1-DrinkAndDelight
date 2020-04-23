b8e6b1e014dc1755bc7ba5bf94bf1c003e5dc036
bharg <bharg@192.168.0.5>
2020-04-23 11:31:52 +0530

    commit #1

---
+++ capg-bvrit-b1-drink-and-delight/src/test/java/com/capg/dnd/CapgBvritB1DrinkAndDelightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.dnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgBvritB1DrinkAndDelightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
