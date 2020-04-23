3f6aa39375e9c313430e0e673b8d6faa59207ec5
NIRANJAN VANAPARTHI <NIRANJAN VANAPARTHI@DESKTOP-M8J4609>
2020-04-23 08:28:15 -0700

    model classes

---
+++ dnd-update-order/src/main/java/com/capg/dnd/updateorder/DndUpdateOrderApplication.java
@@ -0,0 +1,13 @@
+package com.capg.dnd.updateorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndUpdateOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndUpdateOrderApplication.class, args);
+	}
+
+}
